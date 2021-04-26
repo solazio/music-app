32054624651a1937ed1c763ff2fab805784162b9
solazio <ovidiu.suciu@canonical.com>
2021-04-26 03:45:23 +0100

    Remove index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import 'fontsource-roboto';
 import reportWebVitals from './reportWebVitals';
